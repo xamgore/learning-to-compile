ae232d011aa3edd5e8654c03ef367ead9eb2505c
xamgore <xamgore@ya.ru>
2016-10-02 02:34:57 +0300

    Add multiline comment with ids detection

--- src/Step3/ScannerHelper.cs
+++ src/Step3/ScannerHelper.cs
@@ -1,4 +1,5 @@
 ﻿namespace ScannerHelper
 {
-    public enum Tok { EOF = 0, ID, INUM, RNUM, COLON, SEMICOLON, ASSIGN, BEGIN, END, CYCLE };
+    public enum Tok { EOF = 0, COMMENT_START, COMMENT_END,
+        STRING, COMMENT, ID, INUM, RNUM, COLON, SEMICOLON, ASSIGN, BEGIN, END, CYCLE };
 }
